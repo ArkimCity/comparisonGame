aeef7b619b586d54d92914fa08c7c219b4ad02d2
jiwonnn11 <73019862+jiwonnn11@users.noreply.github.com>
2021-01-12 09:49:58 +0900

    Update comparisonDML.sql

--- sql/comparisonDML.sql
+++ sql/comparisonDML.sql
@@ -5,6 +5,10 @@ INSERT INTO gameData VALUES (gameData_dataId_seq.nextval,'강아지 월드컵','
 INSERT INTO gameData VALUES (gameData_dataId_seq.nextval,'강아지 월드컵','닥스훈트','<img src="https://i.ytimg.com/vi/as9jLS78MhA/maxresdefault.jpg">','testParam1_2');
 INSERT INTO gameData VALUES (gameData_dataId_seq.nextval,'강아지 월드컵','진돗개','<img src="https://image.dongascience.com/Photo/2017/06/14964815289707.jpg">','testParam1_3');
 INSERT INTO gameData VALUES (gameData_dataId_seq.nextval,'강아지 월드컵','슈나우저','<img src="https://myanimals.com/ko/wp-content/uploads/2018/04/Schnauzer-dogs-300x200.jpg">','testParam1_4');
+INSERT INTO gameData VALUES (gameData_dataId_seq.nextval,'강아지 월드컵','포메라니안','<img src="https://animalcomp.files.wordpress.com/2017/05/ed8faceba9944.png?w=640">','testParam1_5');
+INSERT INTO gameData VALUES (gameData_dataId_seq.nextval,'강아지 월드컵','치와와','<img src="https://cdn.imweb.me/upload/S201701095872e8d14d5b3/8600d399e3d61.jpg">','testParam1_6');
+INSERT INTO gameData VALUES (gameData_dataId_seq.nextval,'강아지 월드컵','말티즈','<img src="https://babypet.kr/data/file/wiki/3731587261_UuQhJBHg_fa008ba9c8850391d8e46f9593fc6fe221703f75.jpg">','testParam1_7');
+INSERT INTO gameData VALUES (gameData_dataId_seq.nextval,'강아지 월드컵','비숑','<img src="https://post-phinf.pstatic.net/MjAxOTEyMThfOTEg/MDAxNTc2NjQ3ODQ3MDUx.RoJ4Gmr6Equ0y1H18U7nm-w4XJCiKdhRQKx1Q5xM4nQg.kDsNMJZPraTvR-Ce127yDQRKmbpOQcS_a30YaJag3RUg.JPEG/%ED%86%A0%EB%A6%AC.jpg?type=w1200">','testParam1_8');
 
 INSERT INTO gameData VALUES (gameData_dataId_seq.nextval,'라면 월드컵','신라면','<img src="https://www.costco.co.kr/medias/sys_master/images/h64/h96/9867844452382.jpg">','testParam2_1');
 INSERT INTO gameData VALUES (gameData_dataId_seq.nextval,'라면 월드컵','삼양라면','<img src="https://www.samyangfoods.com/upload/product/20180913/20180913135237972039.jpg">','testParam2_2');
